a4f70e44f34a8e68938ce7faf0edd1479066292e
venkatthirupathi <133303811+venkatthirupathi@users.noreply.github.com>
2024-08-26 13:05:51 +0530

    Add files via upload

---
+++ scriptevent.js
@@ -0,0 +1,43 @@
+document.addEventListener('DOMContentLoaded',function()
+{
+    const clickableContainer=document.getElementById('mor1');
+    clickableContainer.addEventListener('click',function(event){
+        if(event.target.classList.contains('mo1'))
+        {
+            window.location.href='mobiles.html';
+        }
+      else   if(event.target.classList.contains('mo2'))
+        {
+            window.location.href='laptopsTablets.html';
+        }
+       else  if(event.target.classList.contains('mo3'))
+        {
+            window.location.href='televisions.html';
+        }
+       else  if(event.target.classList.contains('mo4'))
+        {
+            window.location.href='homeApp.html';
+        }
+       else  if(event.target.classList.contains('mo5'))
+        {
+            window.location.href='kitchenApp.html';
+        }
+       else if(event.target.classList.contains('mo6'))
+        {
+            window.location.href='personals.html';
+        }
+    });
+});
+
+
+
+
+document.getElementById('loginForm').addEventListener('submit', function(event) {
+    event.preventDefault();
+    
+    // Assume successful login
+    let username = document.getElementById('username').value;
+    localStorage.setItem('loggedInUser', username);
+    
+    window.location.href = 'index.html';
+});
